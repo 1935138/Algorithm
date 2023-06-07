2df947f34e48e1bfd78747bc39b65a6f92c8935c
lee1935 <135563454+lee1935@users.noreply.github.com>
2023-06-07 17:24:59 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -2,16 +2,24 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < num; i++) {
 			StringTokenizer stz = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(stz.nextToken());
 			int b = Integer.parseInt(stz.nextToken());
-			System.out.println(a + b);
+			sb.append(a + b).append("\n");
 		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 
 }
