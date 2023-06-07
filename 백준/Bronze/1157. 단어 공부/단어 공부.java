8ad6f35ba6c5912756e352f5008426649e21eb90
lee1935 <135563454+lee1935@users.noreply.github.com>
2023-06-07 09:26:45 +0900

    [Bronze I] Title: 단어 공부, Time: 288 ms, Memory: 21072 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -5,23 +5,29 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine().toUpperCase();
-		
-		char freq_char = '?';
-		int max_count = 0;
-		
-		while(str.length() != 0) {
-			String new_str = str.replaceAll(Character.toString(str.charAt(0)),"");
-			int count = str.length() - new_str.length();
-			if(count > max_count) {
-				freq_char = str.charAt(0);
-				max_count = count;
-			} else if(str.length() - new_str.length() == max_count) {
-				freq_char = '?'; // 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력
-			}
-			str = new_str;
+
+		int[] freq = new int[26];
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			freq[c - 'A']++;
 		}
-		
-		System.out.println(freq_char);
+
+		int max_freq = 0;
+		char max_freq_char = '?';
+
+		for (int i = 0; i < 26; i++) {
+			int count = freq[i];
+			if (count > max_freq) {
+				max_freq = count;
+				max_freq_char = (char) ('A' + i);
+			} else if (count == max_freq) {
+				max_freq = count;
+				max_freq_char = '?';
+			}
+		}
+
+		System.out.println(max_freq_char);
 	}
 
 }
